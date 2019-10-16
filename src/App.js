017d6223c9ce508fbb27f3342557fe7feda48152
Becca <beccaburns89@gmail.com>
2019-10-16 16:23:18 +0200

    Creates Login Form Component. Updates App.js to include login form option.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult';
-import InputFields from './Components/InputFields';
+import InputFields from "./Components/InputFields";
+import LoginForm from './Components/LoginForm';
+
 
 class App extends Component {
   constructor(props) {
@@ -8,27 +10,58 @@ class App extends Component {
     this.state = {
       distance: '',
       gender: 'female',
-      age: ''
+      age: '',
+      renderLoginForm: false,
+      authenticated: false,
+      email: '',
+      password: '',
+      message: ''
     }
   }
 
   onChange(event) {
     this.setState({
       [event.target.id]: event.target.value
-    })
+    });
+  }
+  async onLogin(e) {
+    e.preventDefault();
+    let resp = await authenticate(this.state.email, this.state.password)
+    if (resp.authenticated === true) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: resp.message, renderLoginForm: false })
+    }
   }
 
- render() {
+  render() {
+    let renderLogin
+
+    if (this.state.renderLoginForm === true) {
+      renderLogin = (
+        <LoginForm
+          loginHandler={this.onLogin.bind(this)}
+          inputChangeHandler={this.onChange.bind(this)}
+        />
+      )
+    } else {
+      renderLogin = (
+        <button id="login" onClick={() => this.setState({ renderLoginForm: true })}>Login</button>
+      )
+    }
     return (
       <div>
-        <InputFields
+        <InputFields 
           inputChangeHandler={this.onChange.bind(this)}
-          />
+        />
+
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
+        {renderLogin}
+
       </div>
     );
   }
