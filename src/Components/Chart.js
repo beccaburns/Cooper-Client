09318e1368a96830a5fddf8a15c69bdbbc558d8e
AlexSaxena <saxenaalex@gmail.com>
2019-10-20 00:06:12 +0200

    Adds Constructor to Graph class, Gets and mounts data from performance module, adds bar graph structure.

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react';
-import ChartComponent, {bar} from 'react-chartjs-2';
+import ChartComponent, {Bar} from 'react-chartjs-2';
 import { getData } from '../Modules/PerformanceData';
 
 class Graph extends Component {
-   
+  constructor(props) {
+    super(props);
+    this.state = {
+      performanceData: null
+    }
+  }
+
+  componentDidMount() {
+    this.getPerformanceData()
+  }
+
+  async getPerformanceData() {
+    let result = await getData();
+    this.setState({performanceData: result.data.entries}, () => {
+    })
+  }
+
+   render() {
+     return (
+       <div className="Graph">
+        <Bar
+            data={data} 
+            width={50} 
+            height={100}
+            options={{ maintainAspectRatio: false }}
+        />
+       </div>
+     )
+   }
 }
 
 
