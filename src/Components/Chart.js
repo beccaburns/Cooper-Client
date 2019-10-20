55b28260cca8510eb28b2f89e328ea31e7e14dd7
AlexSaxena <saxenaalex@gmail.com>
2019-10-21 01:04:13 +0200

    Updates Graph Component class and adds graph structure.

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,45 +1,56 @@
 import React, {Component} from 'react';
-import ChartComponent, {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { getData } from '../Modules/PerformanceData';
 
-class Graph extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      performanceData: null
-    }
+class CooperGraph extends Component {
+  state = {
+    performanceData: null
   }
 
-  componentDidMount() {
+  componentDidMount() {   
     this.getPerformanceData()
   }
 
   async getPerformanceData() {
     let result = await getData();
     this.setState({performanceData: result.data.entries}, () => {
+      this.props.resultGraphUpdated();
     })
   }
 
    render() {
-     return (
-       <div className="Graph">
-        <Bar
-            data={data} 
-            width={50} 
-            height={100}
-            options={{ maintainAspectRatio: false }}
-        />
-       </div>
-     )
-   }
+     let CooperGraphData;
+    
+     if (this.props.updateCooperChart === true) {
+       this.getPerformanceData();
+     };
+     
+     if (this.state.performanceData != null) {
+      const distances = []
+      const labels = []
+      this.state.performanceData.forEach(item => {
+        distances.push(item.data.distance)
+        labels.push(item.data.message)
+      })
+      const data = {
+        datasets: [
+          {
+            data: distances,
+            label: 'Perfomance'
+          }
+        ], labels: labels
+      }
+      
+      CooperGraphData = (      
+        <Bar data={data} />
+      )
+    }
+    return (
+      <div>
+        {CooperGraphData}
+      </div>
+    );
+  }
 }
 
-
-
-
-
-
-
-
-
-export default Graph;
+export default CooperGraph;
